ac75fdfac4e6baa38320aba609d653205f590fc4
enfu <516954023@qq.com>
2021-02-24 23:16:24 +0800

    微信支付调试

--- controllers/pay.go
+++ controllers/pay.go
@@ -1,130 +1,106 @@
 package controllers
 
 import (
-	"easy_wallpaper_api/models"
+	"crypto/x509"
+	"encoding/pem"
 	"fmt"
+	"github.com/astaxie/beego"
 	"github.com/iGoogle-ink/gopay"
 	"github.com/iGoogle-ink/gopay/pkg/util"
+	"github.com/iGoogle-ink/gopay/pkg/xlog"
 	"github.com/iGoogle-ink/gopay/wechat/v3"
+	"io/ioutil"
+	"time"
 )
 
 type Pay struct {
-	Base
+	beego.Controller
 }
 
 //定义常量
 //    appId：应用ID
 //    mchId：商户ID
 //    apiKey：API秘钥值
-//    isProd：是否是正式环境
+//    apiV3Key：apiV3Key秘钥
 const (
-	appId  = "wxdaa2ab9ef87b5497"
-	mchId  = "1511774241"
-	apiKey = "J4sQ3YdrgAyrUznO13KKDE7e5D3j1cJz"
+	appId     = "wxdaa2ab9ef87b5497"
+	mchId     = "1511774241"
+	apiV3Key  = "8BDB05l4lVfKQSrJUWSpZgV5eXpI7xm7"
+	serialNo  = "73DAC0D2BC6255926DBBF2BE0135CC6C6F75A4A7"
+	pkContent = ""
 )
-
-// @Title 微信支付
-func (this *Pay) PayAdvanceOrder() {
-	// 初始化微信客户端
-
-	client := wechat.NewClient(appId, mchId, apiKey, false)
-	// 添加微信证书 Path 路径
-	//    certFilePath：apiclient_cert.pem 路径
-	//    keyFilePath：apiclient_key.pem 路径
-	//    pkcs12FilePath：apiclient_cert.p12 路径
-	//    返回err
-	client.AddCertFilePath("config/cert/apiclient_cert.pem", "config/cert/apiclient_key.pem", "config/cert/apiclient_cert.p12")
-
-	// 初始化 BodyMap
-
-	bm := make(gopay.BodyMap)
-	bm.Set("nonce_str", util.GetRandomString(32))
-	bm.Set("body", "小程序测试支付")
-	bm.Set("out_trade_no", "000000")
-	bm.Set("total_fee", 1)
-	bm.Set("spbill_create_ip", "127.0.0.1")
-	bm.Set("notify_url", "http://www.gopay.ink")
-	bm.Set("trade_type", gopay.TradeType_Mini)
-	bm.Set("device_info", "WEB")
-	bm.Set("sign_type", gopay.SignType_MD5)
-	bm.Set("openid", "o0Df70H2Q0fY8JXh1aFPIRyOBgu8")
-
-	// 嵌套json格式数据（例如：H5支付的 scene_info 参数）
-	h5Info := make(map[string]string)
-	h5Info["type"] = "Wap"
-	h5Info["wap_url"] = "http://www.gopay.ink"
-	h5Info["wap_name"] = "H5测试支付"
-
-	sceneInfo := make(map[string]map[string]string)
-	sceneInfo["h5_info"] = h5Info
-
-	bm.Set("scene_info", sceneInfo)
-
-	// 参数 sign ，可单独生成赋值到BodyMap中；也可不传sign参数，client内部会自动获取
-	// 如需单独赋值 sign 参数，需通过下面方法，最后获取sign值并在最后赋值此参数
-	sign := wechat.GetParamSign("wxdaa2ab9ef87b5497", mchId, apiKey, body)
-	// sign, _ := wechat.GetSanBoxParamSign("wxdaa2ab9ef87b5497", mchId, apiKey, body)
-	bm.Set("sign", sign)
-}
 
 // @Title 支付接口
 // @Description 订单支付接口
 // @Param	order_id		query 	int	true		"the order id"
-// @Param	pay_type		query 	int	true		"the pay type"
 // @Success 200 {string} auth success
 // @Failure 403 user not exist
 // @router /pay_order [post]
-func (this *Pay) PayOrder() {
-	// 接收参数 订单id 用户id
-	// 调用微信支付
-	// 回调中处理，更新订单状态，更新支付状态
-
-	orderId := this.GetString("order_id")
-	payType, err := this.GetInt("pay_type")
-
+func (this *Pay) PayAdvanceOrder() {
+	this.getPEM()
+	// 初始化微信客户端
+	// 	serialNo：商户证书的证书序列号
+	//	pkContent：私钥 apiclient_key.pem 读取后的内容
+	client, err := wechat.NewClientV3(appId, mchId, serialNo, apiV3Key, pkContent)
 	if err != nil {
-		this.Data["json"] = ReturnError(40001, "支付单号不能为空")
-		this.ServeJSON()
-		this.StopRun()
+		xlog.Error(err)
+		return
 	}
-	// 查询订单数据
-	info, errs := models.GetNotifyOrdersPay(orderId, this.CurrentLoginUser.Id)
-	if errs != nil {
-		this.Data["json"] = ReturnError(40001, "订单错误或订单不存在")
-		this.ServeJSON()
-		this.StopRun()
+	// 自动验签
+	// 注意：未获取到微信平台公钥时，不要开启，请调用 client.GetPlatformCerts() 获取微信平台公钥
+	//client.AutoVerifySign("微信平台公钥")
+
+	// 打开Debug开关，输出日志
+	//client.DebugSwitch = 1
+
+	tradeNo := util.GetRandomString(32)
+	xlog.Debug("tradeNo:", tradeNo)
+	expire := time.Now().Add(10 * time.Minute).Format(time.RFC3339)
+	// 打开Debug开关，输出日志
+	client.DebugSwitch = gopay.DebugOff
+
+	bm := make(gopay.BodyMap)
+	bm.Set("description", "测试Jsapi支付商品").
+		Set("out_trade_no", tradeNo).
+		Set("time_expire", expire).
+		Set("notify_url", "https://www.gopay.ink").
+		SetBodyMap("amount", func(bm gopay.BodyMap) {
+			bm.Set("total", 1).
+				Set("currency", "CNY")
+		}).
+		SetBodyMap("payer", func(bm gopay.BodyMap) {
+			bm.Set("openid", "asdas")
+		})
+
+	wxRsp, err := client.V3TransactionJsapi(bm)
+	if err != nil {
+		xlog.Error(err)
+		return
 	}
-	fmt.Println(info)
+	if wxRsp.Code == 0 {
+		xlog.Debugf("wxRsp:%#v", wxRsp.Response)
+		return
+	}
+	xlog.Errorf("wxRsp:%s", wxRsp.Error)
+}
 
-	switch payType {
-	case 1: // 信用卡
-		//postdata := make(map[string]interface{})
-		//postdata["orders_code"] = info.OrderId                              //订单号
-		//postdata["order_total"] = (info.TotalPrice + info.TransitPrice)     //支付总金额
-		//postdata["currency_code"] = "USD"                                   //币种，例：美金USD
-		//postdata["order_total_usd"] = (info.TotalPrice + info.TransitPrice) //总折算美金金额
-		//postdata["notify_url"] = Config("notify_url")                       //支付结果回调地址 http://localhost:8055/notify
-		//postdata["products_id"] = info.Id                                   //产品id
-		//postdata["products_name"] = info.Name                               //产品名称
-		//postdata["products_price"] = info.Price                             //产品价格
-		//postdata["products_price_usd"] = info.Price                         //产品折算美金价格
-		//data := GetOrderUrl(postdata)
-
-		// 更新支付单号
-		//boolVal, errVal := models.ModifyOrderTradeNo(info.OrderId, data["orders_id"].(string))
-		//fmt.Println(boolVal)
-		//if errVal == nil && boolVal {
-		//	this.Data["json"] = ReturnSuccess(0, "success", data, 1)
-		//	this.ServeJSON()
-		//} else {
-		//	logs.Error("支付请求错误:" + fmt.Sprintf("%s", errVal))
-		//	this.Data["json"] = ReturnError(40003, "支付请求错误")
-		//	this.ServeJSON()
-		//}
-		break
-	default:
-		this.Data["json"] = ReturnError(40004, "支付通道暂未开通")
-		this.ServeJSON()
+func (this *Pay) getPEM() {
+	certFile, err := ioutil.ReadFile("conf/cert/apiclient_key.pem")
+	if err != nil {
+		fmt.Println(err.Error())
 	}
 
+	pemBlock, _ := pem.Decode([]byte(certFile))
+	if pemBlock == nil {
+		fmt.Println("decode error")
+	}
+
+	cert, err := x509.ParseCertificate(pemBlock.Bytes)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	fmt.Printf("Name %s\n", cert.Subject.CommonName)
+	fmt.Printf("Not before %s\n", cert.NotBefore.String())
+	fmt.Printf("Not after %s\n", cert.NotAfter.String())
 }
