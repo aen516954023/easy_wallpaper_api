9c4faeb12346565cc958e10e753a5ef145f33b4c
enfu <516954023@qq.com>
2021-06-05 10:30:52 +0800

    modify api option

--- models/memberormasterworker.go
+++ models/memberormasterworker.go
@@ -74,7 +74,7 @@ type OrderWorkerList struct {
 func GetOrderWorkerList(orderId int) (int64, []OrderWorkerList, error) {
 	o := orm.NewOrm()
 	var data []OrderWorkerList
-	num, err := o.Raw("SELECT w.w_id as id,m.username as name,m.image,m.is_real_name,m.exp,m.warranty FROM e_member_or_master_worker w LEFT JOIN e_master_worker m ON m.id=w.w_id WHERE w.o_id=?", orderId).QueryRows(&data)
+	num, err := o.Raw("SELECT w.w_id as id,m.username as name,m.image,m.is_real_name,m.exp,m.warranty FROM e_member_or_master_worker w LEFT JOIN e_master_worker m ON m.id=w.w_id WHERE w.o_id=? and w.status >= 0", orderId).QueryRows(&data)
 	return num, data, err
 }
 
